8fc5ae3a125d5703dca71187b9f7ad361ad7c3f6
sebastianseegy <sebastianseegy@gmail.com>
2026-03-31 21:36:16 +0200

    Change Imprint

--- src/pages/Impressum.tsx
+++ src/pages/Impressum.tsx
@@ -1,24 +1,29 @@
 export default function Impressum() {
   return (
-    <div className="max-w-3xl mx-auto text-left flex flex-col gap-6">
+    <div className="max-w-3xl mx-auto text-left flex flex-col gap-6 text-sm md:text-base">
       <h1 className="text-2xl mb-8 text-center">IMPRESSUM</h1>
       <p>ANGABEN GEMÄSS § 5 TMG</p>
       <p>
-        Restauration19<br/>
+        Hermanns&amp;Rützel GbR<br/>
         Helmstraße 19<br/>
         90419 Nürnberg
       </p>
       <p>
         VERTRETEN DURCH:<br/>
-        Flo, Krispin, Pascal
+        Florian Hermanns, Kristin Rützel
       </p>
       <p>
         KONTAKT:<br/>
-        E-Mail: info@restauration19.de
+        Telefon: 0049 911<br/>
+        E-Mail: <a href="mailto:info@restauration19.de" className="hover:text-brand-pink transition-colors">info@restauration19.de</a>
       </p>
       <p>
-        Umsatzsteuer-Identifikationsnummer gemäß § 27 a Umsatzsteuergesetz:<br/>
-        DE XXX XXX XXX
+        AUFSICHTSBEHÖRDE:<br/>
+        Nürnberg
+      </p>
+      <p>
+        STEUERNUMMER GEMÄSS § 21 UMSATZSTEUERGESETZ:<br/>
+        238/162/50161
       </p>
     </div>
   );
