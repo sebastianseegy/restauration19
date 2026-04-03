b4e96e35b3dd183b2ede256a8bca0c9d00c06ac6
sebastianseegy <sebastianseegy@gmail.com>
2026-04-03 12:55:07 +0200

    Firebase strict react domain changen and SEO

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,14 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// SEO-Optimierung: Verhindert "Duplicate Content" über die Firebase-Standard-Domains
+if (
+  window.location.hostname === 'restauration19-fde4f.web.app' || 
+  window.location.hostname === 'restauration19-fde4f.firebaseapp.com'
+) {
+  window.location.replace(`https://www.restauration19.de${window.location.pathname}${window.location.search}`);
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
